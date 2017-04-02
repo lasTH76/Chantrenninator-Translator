8a3fe04f6f4f60fc183fdb5fdd54530b92c9c689
lasTH76 <louisregno@gmail.com>
2017-04-02 19:37:59 +0200

    Creation des listes de mots et petites modifs sur les phrases

--- traducteur.py
+++ traducteur.py
@@ -1,13 +1,18 @@
-import os
+import os, random
 os.chdir('C:\Python34')
-# -*- coding: Unicode -*-
+
 fA = open('VocaAnglais.txt', 'r')
 fF = open('VocaFrancais.txt', 'r')
 
-lstA = fA.readlines()
-lstF = fF.readlines()
+listfF = [line.split('\n') for line in fF.readlines()]
+listfA = [line.split('\n') for line in fA.readlines()]
 
-print(lstA)
+print("Bienvenue dans ce jeu de traduction mon jeune ami. Comment t'appelles-tu? ")
+prenom = input("Veuillez le renseigner de ce côté --> ")
+print("Très bien mon jeune ", prenom, " .")
+print("Tu vas maintenant avoir 15 mots à traduire.")
+
+print(len(listfF))
 
 fA.close()
 fF.close()
