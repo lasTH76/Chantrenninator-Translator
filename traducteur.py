36154c0fb5d0804167d0bf8b06e882e785ada824
lasTH76 <louisregno@gmail.com>
2017-04-04 12:02:05 +0200

    changement programme de tram

--- traducteur.py
+++ traducteur.py
@@ -1,11 +1,22 @@
-import os
-os.chdir('C:\Python34')
-# -*- coding: Unicode -*-
+import os,sys
+
+def SearchPath():
+   if getattr(sys, 'frozen', False):
+      datadir = os.path.dirname(sys.executable)
+   else:
+      datadir = os.path.dirname(__file__)
+   return datadir
+
+
+
+chemin = SearchPath()
+CheminAbsolu = os.path.join(chemin, "Chantrenninator-Translator")
+
 fA = open('VocaAnglais.txt', 'r')
 fF = open('VocaFrancais.txt', 'r')
 
-lstA = fA.readlines()
-lstF = fF.readlines()
+lstA = [line.split('/n') for line in fA.readlines()]
+lstF = [line.split('/n') for line in fF.readlines()]
 
 print(lstA)
 
