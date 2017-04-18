8c90d817ca5992920f6ceec14c1aa54c5fac0007
lasTH76 <louisregno@gmail.com>
2017-04-18 17:26:36 +0200

    changement programme trad

--- traducteur.py
+++ traducteur.py
@@ -20,13 +20,12 @@ listfA = [line.split('\n') for line in fA.readlines()]
 
 print("Bienvenue dans ce jeu de traduction mon jeune ami. Comment t'appelles-tu? ")
 prenom = input("Veuillez le renseigner de ce côté --> ")
-print("Très bien mon jeune ", prenom, " .")
+print("Très bien mon jeune ", prenom, ".")
 print("Tu vas maintenant avoir 15 mots à traduire.")
 
-lstA = [line.split('/n') for line in fA.readlines()]
-lstF = [line.split('/n') for line in fF.readlines()]
 
-print(len(listfF), len(listfA))
+print(len(listfA))
+print(len(listfF))
 
 fA.close()
 fF.close()
