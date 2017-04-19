4851289cce2431628f15f1345881ccf6601efe43
lasTH76 <louisregno@gmail.com>
2017-04-19 17:21:52 +0200

    création du choix et essai pour le fr

--- traducteur.py
+++ traducteur.py
@@ -15,17 +15,35 @@ CheminAbsolu = os.path.join(chemin, "Chantrenninator-Translator")
 fA = open('VocaAnglais.txt', 'r')
 fF = open('VocaFrancais.txt', 'r')
 
-listfF = [line.split('\n') for line in fF.readlines()]
-listfA = [line.split('\n') for line in fA.readlines()]
+listfF = [line.split("/n") for line in fF.readlines()]
+listfA = [line.split("/n") for line in fA.readlines()]
+
+score = 0
+nbrEssai = 0
 
 print("Bienvenue dans ce jeu de traduction mon jeune ami. Comment t'appelles-tu? ")
 prenom = input("Veuillez le renseigner de ce côté --> ")
-print("Très bien mon jeune ", prenom, ".")
+print("Très bien mon jeune", prenom, ".")
 print("Tu vas maintenant avoir 15 mots à traduire.")
+print("Fais attention aux accents.")
+print("Maintenant, dans quelle langue veux-tu jouer?")
+choix = input("Français ou Anglais?    ")
 
+if choix == "Français":
+   print("Il faut les traduire en anglais")
+   
+   for loop in range(15):
+      hasard = random.randint(0, len(listfF))
+      mot = listfF[hasard]
+      print(str(mot))
 
-print(len(listfA))
-print(len(listfF))
+if choix == "Anglais":
+   print("Il faut les traduire en français")
+   
+
+else:
+   while not("Français") or not("Anglais"):
+      choix
 
 fA.close()
 fF.close()
