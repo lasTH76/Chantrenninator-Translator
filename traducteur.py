47696b512cfdaf97dd25fa512b2d2a03fd638a69
lasTH76 <louisregno@gmail.com>
2017-04-20 10:16:35 +0200

    Jeu de traduction fini

--- traducteur.py
+++ traducteur.py
@@ -1,49 +1,108 @@
-import os, random, sys
-os.chdir('C:\Python34')
+import os, random, sys, time
+
+#os.chdir('C:\Python34')
 
 def SearchPath():
-   if getattr(sys, 'frozen', False):
-      datadir = os.path.dirname(sys.executable)
-   else:
-      datadir = os.path.dirname(__file__)
-   return datadir
+	if getattr(sys, 'frozen', False):
+		datadir = os.path.dirname(sys.executable)
+	else:
+		datadir = os.path.dirname(__file__)
+	return datadir
 
 
 chemin = SearchPath()
 CheminAbsolu = os.path.join(chemin, "Chantrenninator-Translator")
 
-fA = open('VocaAnglais.txt', 'r')
-fF = open('VocaFrancais.txt', 'r')
+fA = open(os.path.join(chemin, 'VocaAnglais.txt'), 'r')
+fF = open(os.path.join(chemin, 'VocaFrancais.txt'), 'r')
 
 listfF = [line.split("/n") for line in fF.readlines()]
 listfA = [line.split("/n") for line in fA.readlines()]
 
 score = 0
-nbrEssai = 0
+nbrEssai = 5
+choix = "Vide"
+ReponseJoueur = "mouais"
+nbMot = 0
 
 print("Bienvenue dans ce jeu de traduction mon jeune ami. Comment t'appelles-tu? ")
-prenom = input("Veuillez le renseigner de ce côté --> ")
+prenom = input("Veuillez le renseigner de ce côté -->   ")
 print("Très bien mon jeune", prenom, ".")
-print("Tu vas maintenant avoir 15 mots à traduire.")
-print("Fais attention aux accents.")
+
+while nbMot <= 4:
+	nbMot = int(input("Indiquer le nombre de mots que vous voulez traduire -->   "))
+	if nbMot > 0:
+		print("Petit Joueur.", end='\n')
+
+print("Tu vas maintenant avoir", nbMot, "mots à traduire.")
+print("Fais attention il n'y a pas d'accent.")
 print("Maintenant, dans quelle langue veux-tu jouer?")
-choix = input("Français ou Anglais?    ")
 
-if choix == "Français":
-   print("Il faut les traduire en anglais")
-   
-   for loop in range(15):
-      hasard = random.randint(0, len(listfF))
-      mot = listfF[hasard]
-      print(str(mot))
 
-if choix == "Anglais":
-   print("Il faut les traduire en français")
-   
+def repareMot(mot):
+	listeNouvMot = []
+	ignoreSuivant = False
+	for indLettre in range(len(mot)):
+		if ignoreSuivant:
+			ignoreSuivant = False
+			pass
+		elif (mot[indLettre] in ("[", "]", "'")):
+			pass
+		elif mot[indLettre] == chr(92) and mot[indLettre + 1] == "n":
+			ignoreSuivant = True
+			pass
+		else:
+			listeNouvMot.append(mot[indLettre])
+	nouveauMot = "".join(listeNouvMot)
+	nouveauMot.replace("\n", "")
+	if nouveauMot.endswith("\n"):
+		vraiMot = nouveauMot[:-1] #On enleve le dernier caractere (= saut de ligne) pour eviter les sauts de lignes inutiles
+	else:
+		vraiMot = nouveauMot
+	return vraiMot
 
-else:
-   while not("Français") or not("Anglais"):
-      choix
+while not choix in ("Francais", "Anglais"):
+	choix = input("Francais ou Anglais?    ")
+	if choix == "Francais":
+		print("Il faut les traduire en anglais")
+		for loop in range(nbMot):
+			hasard = random.randint(0, len(listfF))
+			mot = repareMot(listfF[hasard])
+			reponse = repareMot(listfA[hasard])
+			reponse.lower()
+			print(mot, end='\n')
+			while ReponseJoueur != reponse and nbrEssai > 0:
+				ReponseJoueur = input()
+				if ReponseJoueur != reponse:
+					nbrEssai += -1
+					print("Il vous reste", nbrEssai, "essais.", end= '\n')
+				if ReponseJoueur == reponse and nbrEssai >= 0:
+					score += 1
+					break
+			nbrEssai = 5
+
+
+	if choix == "Anglais":
+		print("Il faut les traduire en francais")
+		for loop in range(nbMot):
+			hasard = random.randint(0, len(listfA))
+			mot = repareMot(listfA[hasard])
+			reponse = repareMot(listfF[hasard])
+			reponse.lower()
+			print(mot, end='\n')
+			while ReponseJoueur != reponse and nbrEssai > 0:
+				ReponseJoueur = input()
+				if ReponseJoueur != reponse:
+					nbrEssai += -1
+					print("Il vous reste", nbrEssai, "essais.", end= '\n')
+				if ReponseJoueur == reponse and nbrEssai >= 0:
+					score += 1
+					break
+			nbrEssai = 5
+
+print(prenom, ",vous avez un score de",score,"sur",(loop +1))
+
 
 fA.close()
 fF.close()
+time.sleep(1)
