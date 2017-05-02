bf34481f46dfd4e0275393f7dc6b49996a60f90a
lasTH76 <louisregno@gmail.com>
2017-05-02 12:03:39 +0200

    Essai teinté

--- Traduction/Tkinter.py
+++ Traduction/Tkinter.py
@@ -1,7 +1,12 @@
 from tkinter import *
-import traducteur.py, binaire.py, Morse.py
+import traducteur.py as trad
+import JeuMorse.py as morse
+
+fenetre1 = Tk()
+fenetre1.title('Jeux de traductions Morse et Fr/An')
+
+interface = Interface(fenetre1)
+Bouton1 = Button(fentetre1, text = 'Jeu de traduction Fr/An', command = Frame.trad)
 
 
-fenetre = Tk()
-
-Bouton1 = Button(fenetre, 
+interface.mainloop()
