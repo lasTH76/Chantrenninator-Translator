5d224f5a1cb03550d4b07a1417f73a3c73e85d5b
juscar <juscar76@gmail.com>
2017-05-09 11:23:13 +0200

    Add files via upload

--- Oscar/choix.py
+++ Oscar/choix.py
@@ -1,36 +1,62 @@
 from tkinter import *
 import tkinter as tk
 import random
+import copy
 #-------------------------------------------
 
 windo = Tk()
 windo.configure(width=600, height=375)
 windo.title("trouve la capitale")
-windo.resizable(width=False, height=False)
+
 #------------------------------------------
+
+modeJeu = "Europe"
+
+def changeMode(nouvMode):
+    global modeJeu
+    modejeu = nouvMode
+
 def France():
-    global paysEurope, capitaleEurope 
+    global paysEurope, capitaleEurope
+
+def Monde():
+    pass
     
 def Europe():
-    global paysEurope, capitaleEurope, nombre, RP,compteur_essai, correct, choix, reponse
+    global paysEurope, capitaleEurope, nombre, RP, compteur_essai, correct, choix, Nbreussite, Capitale, Region
+    Capitale=copy.copy(capitaleEurope)
+    Region=copy.copy(paysEurope)
     for loop in range(nombre):
-        RP=randint(0,26-loop)
+        RP=random.randint(0,26-loop)
         correct=False
         compteur_essai = 0
-        while correct==False and compteur_essai != 3:
-            print("Quelle est la capitale de ce pays : ",paysEurope[RP])
-            choix=reponse
-            if choix ==capitaleEurope[RP]:
-                print("tu as donné la bonne réponse!")
+        while not correct and compteur_essai < 3:
+            tot3.config(text = "Quelle est la capitale de ce pays : {}".format(Region[RP]))
+            choix= reponse.get()
+            if choix ==Capitale[RP]:
+                tot2.config(text ="tu as donné la bonne réponse!")
                 correct=True
                 compteur_essai = 3
                 Nbreussite=Nbreussite+1
-            elif choix != copie_capitale[RP] and compteur_essai < 3 :
-                print("C'est faux")
+            elif choix != Capitale[RP]:
+                tot2.config(text = "C'est faux")
                 correct=False
                 compteur_essai = compteur_essai + 1
-        copie_pays.remove(copie_pays[RP])
-        copie_capitale.remove(copie_capitale[RP])
+                nbreponce=Label(windo, text="il te reste {} essais".format(compteur_essai), font = "Helvetica 20 bold")
+                nbreponce.place(x=210, y=120)
+                nbreponce.config(text ="il te reste {} essais".format(compteur_essai))
+        Region.remove(Region[RP])
+        Capitale.remove(Capitale[RP])
+
+def Valide():
+    if modeJeu == "Europe":
+        Europe()
+    elif modeJeu == "France":
+        France()
+    elif modeJeu == "Monde":
+        Monde()
+    else:
+        raise ValueError("Mauvaise valeur pour modeJeu")
 
 
         
@@ -50,9 +76,10 @@ def nbquestion4():
 menubar = Menu(windo)
 
 menu1 = Menu(menubar, tearoff=0)
-menu1.add_command(label="France")
-menu1.add_command(label="Europe")
-menubar.add_cascade(label="Choi de la Region",menu=menu1)
+menu1.add_command(label="France", command = lambda : changeMode("France"))
+menu1.add_command(label="Europe", command = lambda : changeMode("Europe"))
+menu1.add_command(label = "Monde", command = lambda: changeMode("Monde"))
+menubar.add_cascade(label="Choix de la Region",menu=menu1)
 
 menu2 = Menu(menubar, tearoff=0)
 menu2.add_command(label="5",command=nbquestion1)
@@ -74,26 +101,32 @@ capitaleEurope=["Berlin","Vienne","Bruxelles","Sofia","Nicosie","Copenhague","Ma
 regionFrance=["Auvergne-Rhône-Alpes","Bourgogne-Franche-Comté ","Bretagne","Centre-Val-de-Loire","Corse","Grand_Est","Hauts-De-France", "île-de-France","Normandie","Nouvelle-Aquitaine","Occitane","pays_de_la_Loire","Provence-Alpes-Côtes_d’Azur","Guadeloupe","Guyane","Martinique","Reunion","Mayotte"]
 capitaleFrance=["Lyon","Dijon","Brest","Orléans","Ajaccio","Strasbourg","Lille","Paris","Rouen","Bordeaux","Toulouse","Nantes","Marseille","Basse-Terre","Fort-de-France","cayenne","Saint-Denis","Dzaoudzi"]
 
+Region=[]
+Capitale=[]
+
 nombre=5
 RP=""
 correct=False
+Nbreussite=0
+compteur_essai=0
+
 #-----------------------------------------
 #on fait l'interface graphic
 
 reponse = StringVar()
-reponsa = Entry(windo, textvariable = reponse, width=21, font="Helvetica 15 bold", justify=CENTER)
-reponsa.place(x=205, y=160)
+reponse = Entry(windo, textvariable = reponse, width=21, font="Helvetica 15 bold", justify=CENTER)
+reponse.place(x=205, y=160)
 
-boutonreni=Button(windo, text="Valider", padx=8, pady=3, font = "Helvetica 10 bold", relief=GROOVE, activebackground="#A4A4A4")
-boutonreni.place(x=270, y=200)
+boutonreni=Button(windo, text="Valider", padx=8, pady=3, font = "Helvetica 10 bold", relief=GROOVE, activebackground="#A4A4A4", command = Valide)
+boutonreni.place(x=270, y=300)
 
 tot2=Label(windo, text="", font = "Helvetica 20 bold")
-tot2.place(x=250, y=120)
+tot2.place(x=250, y=200)
 
 tot3=Label(windo, text="", font = "Helvetica 20 bold")
-tot3.place(x=250, y=90)
+tot3.place(x=150, y=90)
 
-nbreponce=Label(windo, text="il te restse 3 essais", font = "Helvetica 20 bold")
-nbreponce.place(x=250, y=120)
+#nbreponce=Label(windo, text="il te reste " + str(compteur_essai)+ " essais", font = "Helvetica 20 bold")
+#nbreponce.place(x=210, y=120)
 #-----fin
 windo.mainloop()
