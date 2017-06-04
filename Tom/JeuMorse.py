d240796ca15636af0a10ea4c8e32a61e1a335769
lasTH76 <louisregno@gmail.com>
2017-06-04 17:04:08 +0200

    Fin du jeu de Morse

--- Tom/JeuMorse.py
+++ Tom/JeuMorse.py
@@ -5,8 +5,11 @@ from tkinter import *
 fenetreJeu2 = Tk()
 fenetreJeu2.title('Le Morse')
 fenetreJeu2['bg'] = 'grey'
-fenetreJeu2.geometry('500x500+750+350')
+fenetreJeu2.geometry('900x500+650+350')
 #_______________________________________________________________
+def showScore():
+    pass
+
 def reportf():
     reportf=Toplevel()
     reportf.configure(width=600, height=300)
@@ -51,7 +54,7 @@ def créateurs():
     createurs.configure(width = 1666, height = 1666)
     createurs.title('Générique de Fin')
     createurs.resizable(False, False)
-    createurs['bg'] = fenetreJeu['bg']
+    createurs['bg'] = fenetreJeu2['bg']
     
     labelC1 = Label(createurs, text = 'Ce jeu vous a été présenté par ' + '\n' + 'Tom Habbar, Dylan Essakhi et Oscar Sellier.' + '\n' + "Avec l'aide supplémentaire de Gawein Le Goff et de Erwan Castioni" + '\n' + "A l'occasion du bac d'ISN se déroulant au Lycée Camille Saint-Saens" + '\n' + "En espérant qu'il vous plaise ^^", font='Helvetica 15 bold', justify=CENTER)
     labelC1.pack()
@@ -60,7 +63,7 @@ def créateurs():
 menubarre = Menu(fenetreJeu2)
 
 menu1 = Menu(menubarre, tearoff = 0)
-##        menu1.add_command(label='Scores', command = showScore)
+menu1.add_command(label='Scores', command = showScore)
 menu1.add_separator()
 menu1.add_command(label='Quitter', command = fenetreJeu2.quit)
 menubarre.add_cascade(label='Fichier', menu=menu1)
@@ -94,6 +97,17 @@ Morse = {'A': '.-', 'B': '-...', 'C':'-.-.', 'D': '-..',
 
 answer = StringVar()
 result = 0
+Indication = Label(fenetreJeu2)
+BonneRep = Label(fenetreJeu2)
+MauvaiseRep = Label(fenetreJeu2)
+compteurMot = 0
+goodanswer = ''
+Try = 3
+zoneRep = Entry(fenetreJeu2)
+
+labelMot = Label(fenetreJeu2)
+zoneRep = Entry(fenetreJeu2)
+labelVide2 = Label(fenetreJeu2)
 
 prenom = Label(fenetreJeu2, text = "Indiquez votre nom ici", font='Helvetica 12 bold', justify=CENTER)
 prenom.pack()
@@ -119,39 +133,86 @@ EntreeNbr = Entry(fenetreJeu2, textvariable = number, font='Helvetica 12 bold', 
 
 def Accepter_2(evnt):
     global label1, label2, label3, EntreeNbr
-    label1.destroy()
-    label2.destroy()
-    EntreeNbr.destroy()
-    label3.pack()
+    if number.get() >= 2:
+        label1.destroy()
+        label2.destroy()
+        EntreeNbr.destroy()
+        label3.pack()
+        labelVide.pack()
+        Game()
+    else:
+        number.set('2')
 
-label3 = Label(fenetreJeu2, text = "It's time to start!" + '\n', font='Helvetica 12 bold', justify=CENTER)
-label3.bind('<KeyPress-Return>', Accepter_2)
-nombre = number.get()
+label3 = Label(fenetreJeu2, text = "It's time to start!", font='Helvetica 12 bold', justify=CENTER)
+labelVide = Label(fenetreJeu2, text = '\n', font='Helvetica 12 bold', justify=CENTER, bg = fenetreJeu2['bg'])
+EntreeNbr.bind('<KeyPress-Return>', Accepter_2)
 
-for loop in range(nombre):
+def AccepterRep(event):
+    global Indication, MauvaiseRep, BonneRep, zoneRep, labelMot, goodanswer, compteurMot, result, Try, answer, boutonSuivant, labelVide2
+    if answer.get() != goodanswer and Try > 0:
+        Indication.destroy()
+        Try -= 1
+        Indication = Label(fenetreJeu2, text = str(Try) + " essai(s) restant(s).", font='Helvetica 12 bold', justify=CENTER)
+        Indication.pack()
+        answer.set('')
+    if answer.get() == goodanswer:
+        Indication.destroy()
+        result += 1
+        BonneRep = Label(fenetreJeu2, text ="Bonne réponse!", font='Helvetica 12 bold', justify=CENTER)
+        BonneRep.pack()
+        labelVide2 = Label(fenetreJeu2, text = '\n', font='Helvetica 12 bold', justify=CENTER, bg = fenetreJeu2['bg'])
+        labelVide2.pack()
+        labelMot.destroy()
+        zoneRep.destroy()
+        answer.set('')
+        boutonSuivant.pack()
+    if Try == 0 and answer.get() != goodanswer:
+        Indication.destroy()
+        MauvaiseRep = Label(fenetreJeu2, text = "La réponse était " + goodanswer + "." + '\n', font='Helvetica 12 bold', justify=CENTER)
+        MauvaiseRep.pack()
+        labelVide2 = Label(fenetreJeu2, text = '\n', font='Helvetica 12 bold', justify=CENTER, bg = fenetreJeu2['bg'])
+        labelVide2.pack()
+        labelMot.destroy()
+        zoneRep.destroy()
+        answer.set('')
+        boutonSuivant.pack()
+
+def Game():
+    global compteurMot, goodanswer, BonneRep, MauvaiseRep, Indication, zoneRep, labelMot, number, boutonSuivant, labelVide2
+
+    if compteurMot != number.get():
+        labelVide2.destroy()
+        boutonSuivant.pack_forget()
+        labelMot.destroy()
+        zoneRep.destroy()
+        Indication.destroy()
+        MauvaiseRep.destroy()
+        BonneRep.destroy()
+
         letter = random.randint(33,90)
         while (letter > 34 and letter < 39) or (letter > 41 and letter < 44) or (letter > 46 and letter < 48) or (letter > 59 and letter < 63) or (letter > 63 and letter < 65):
                 letter = random.randint(33,90)
+                
         goodanswer = Morse[chr(letter).upper()]
         Try = 3
+        compteurMot += 1
         
         labelMot = Label(fenetreJeu2, text = chr(letter), font='Helvetica 12 bold', justify=CENTER)
         labelMot.pack()
+        zoneRep = Entry(fenetreJeu2, textvariable = answer, font ='Helvetica 12 bold', justify=CENTER)
+        zoneRep.pack()
+        zoneRep.bind('<KeyPress-Return>', AccepterRep)
 
-        def AccpterRep(event):
-            if answer != goodanswer and Try > 0:
-                Indication.destroy()
-                Try += -1
-                Indication = Label(fenetreJeu2, text = Try + " essai(s) restant(s).", font='Helvetica 12 bold', justify=CENTER)
-                Indication.pack()
-            if answer.get() == goodanswer:
-                    result += 1
-                    BonneRep = Label(fenetreJeu2, text ="Bonne réponse!", font='Helvetica 12 bold', justify=CENTER)
-                    BonneRep.pack()
-            if Try == 0 and answer.get() != goodanswer:
-                    MauvaiseRep = Label(fenetreJeu2, text = "La réponse était " + goodanswer + ".", font='Helvetica 12 bold', justify=CENTER)
-                    MauvaiseRep.pack()
-##        print(prenom, "has", result, "over", number, ".")
-##
-##if __name__ == '__main__':
-##        morse()
+    elif compteurMot == number. get():
+        boutonSuivant.destroy()
+        Indication.destroy()
+        MauvaiseRep.destroy()
+        BonneRep.destroy()
+        labelMot.destroy()
+        zoneRep.destroy()
+        labelFinal = Label(fenetreJeu2, text = ValeurPrenom.get() + ", vous avez "+ str(result) + " sur " + str(number.get()) + ".", font='Helvetica 12 bold', justify=CENTER)
+        labelFinal.pack()
+
+boutonSuivant = Button(fenetreJeu2, text = 'Suivant', command = Game, font='Helvetica 12 bold', justify=CENTER)
+
+fenetreJeu2.mainloop()
